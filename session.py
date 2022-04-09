f1c09cca3184c8081fad3280a23f2e2bb4ed446f
SUKHPAL <86770159+SUKHPAL443@users.noreply.github.com>
2022-04-09 16:14:47 +0530

    Update session.py

--- session.py
+++ session.py
@@ -3,13 +3,13 @@ from telethon.sync import TelegramClient
 
 
 print(
-    """\n.##.....##.####..######...##.....##.########.##....##
-.###...###..##..##....##..##.....##....##.....##..##.
-.####.####..##..##........##.....##....##......####..
-.##.###.##..##..##...####.#########....##.......##...
-.##.....##..##..##....##..##.....##....##.......##...
-.##.....##..##..##....##..##.....##....##.......##...
-.##.....##.####..######...##.....##....##.......##..."""
+    """\n.
+╔══╗╔╗──╔═══╦═══╦╗╔═╗
+║╔╗║║║──║╔═╗║╔═╗║║║╔╝
+║╚╝╚╣║──║║─║║║─╚╣╚╝╝
+║╔═╗║║─╔╣╚═╝║║─╔╣╔╗║
+║╚═╝║╚═╝║╔═╗║╚═╝║║║╚╗
+╚═══╩═══╩╝─╚╩═══╩╝╚═╝"""
 )
 
 print("\n • Telethon String Session •")
@@ -22,9 +22,9 @@ API_HASH = input("\nEnter API HASH Here : ")
 
 
 try:
-   with TelegramClient(StringSession(), APP_ID, API_HASH) as Mighty:
+   with TelegramClient(StringSession(), APP_ID, API_HASH) as Black:
        print("\nSTRING SESSION GENERATED SUCCESSFULLY !!\nCHECK YOUR SAVED MASSAGES.\nString is a Sensitive Data, Don't Share it With Anyone. ")
-       Mighty.send_message("me", f"**Mighty X Spam Session :**\n\n`{Mighty.session.save()}`\n\n__Don't Share it With Anyone.__")
+       Black.send_message("me", f"**Black X Spam Session :**\n\n`{Black.session.save()}`\n\n__Don't Share it With Anyone.__")
        
 except Exception as e:
     print(f"{e}") 
